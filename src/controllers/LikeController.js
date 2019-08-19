3f4c25b456af244922e5b3b6ebf744cf6a434d0f
Anologicon <pauloraimundi2009@gmail.com>
2019-08-19 00:24:42 -0300

    Match

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -2,6 +2,9 @@ const Dev = require('../model/Dev');
 
 module.exports = {
     async store(req, res) {
+        console.log(req.io, req.connectedUsers);
+        
+
         const { user } = req.headers;
         const { devId } = req.params;
 
@@ -15,7 +18,16 @@ module.exports = {
         }
 
         if (targetDev.likes.includes(loggedDev._id)) {
-            console.log('Match!');
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if (loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
         loggedDev.likes.push(targetDev._id);
--- src/server.js
+++ src/server.js
@@ -3,15 +3,35 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+       
+
+    connectedUsers[user] = socket.id
+    
+});
 
 mongoose.connect('mongodb+srv://anologiconTinder:anologiconTinder@todolist-wmz4g.mongodb.net/omnistackTinder?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(7100);
 
